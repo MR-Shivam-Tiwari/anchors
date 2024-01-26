92b1947a7278cf20cc6c36c7e3b8f7362a425fc2
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-01-26 22:40:14 +0530

    Update Register.jsx

--- Frontend/src/Components/Auth/Register.jsx
+++ Frontend/src/Components/Auth/Register.jsx
@@ -12,7 +12,7 @@ function Register({ isDarkMode }) {
 
   const handleRegisterClick = async () => {
     try {
-      const response = await fetch("http://localhost:3002/register", {
+      const response = await fetch("https://anchorsbackend-seven.vercel.app/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
